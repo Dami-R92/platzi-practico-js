a059afde95f3b0a15dae8a5cd0135ee159fd2e82
Damián Rojas <drojas@stilowg.local>
2021-08-03 08:41:07 -0300

    Calculador de forma geometricas

--- myApp/figurasLogica.js
+++ myApp/figurasLogica.js
@@ -1,64 +1,85 @@
 //CODIGO DEL CUADRADO.
-
 console.group('Cuadrado')
+const input = document.getElementById("InputCuadrado");
 
-const ladoCuadrado = 5;
+let perimetroCuadrado = (lado) => {
+    return lado * 4;
+}
 
-console.log('Los lados del cuadrado miden: ' + ladoCuadrado + 'cm');
+let areaCuadrado = (lado) => {
+    return lado * lado
+}
 
-const perimetroCuadrado = ladoCuadrado * 4;
+let calcularPerimetroCuadrado = () => {
+    const value = input.value;
+    let perimetro = perimetroCuadrado(value);
+    alert(`El perimetro de su cuadrado es de ${perimetro} cm.`)
+}
 
-console.log(`El perimetro de un cuadrado es igual a: ${perimetroCuadrado} cm`);
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-
-console.log(`El Area de un cuadrado es igual a: ${areaCuadrado} cm2`);
-
+let calcularAreaCuadrado = () => {
+    const value = input.value;
+    let area = areaCuadrado(value);
+    alert(`El Area de su cuadrado es de ${area} cm2.`)
+}
 console.groupEnd();
-
-//CODIGO DEL TRIANGULO
 
 console.group('Triangulo')
 
-const ladoTrianguloA = 6;
-const ladoTrianguloB = 6;
-const BaseTriangulo = 4;
-const alturaTriangulo = 5.5;
-const perimetroTriangulo = ladoTrianguloA + ladoTrianguloB + BaseTriangulo;
-const areaTriangulo = (BaseTriangulo * alturaTriangulo) / 2;
+//CONDIGO DE TRIANGULO
+const inputA = document.getElementById("InputTrianguloA");
+const inputB = document.getElementById("InputTrianguloB");
+const inputC = document.getElementById("InputTrianguloC");
+const inputH = document.getElementById("InputTrianguloH");
 
-console.log(`Los lados del triangulo miden: Lado A ${ladoTrianguloA} cm, Lado B ${ladoTrianguloB} cm, Base ${BaseTriangulo} cm`);
+let perimetroTriangulo = (ladoA, ladoB, base) => {
+    return ladoA + ladoB + base;
+}
+let areaTriangulo = (base, altura) => {
+    return (base * altura) / 2;
+}
+let calcularPerimetroTriangulo = () => {
+    const valueA = parseInt(inputA.value);
+    const valueB = parseInt(inputB.value);
+    const valueC = parseInt(inputC.value);
 
-console.log(`La altura de un triangulo es igual a: ${alturaTriangulo} cm`);
+    let perimetro = perimetroTriangulo(valueA, valueB, valueC);
+    alert(`El perimetro de su triangulo es de ${perimetro} cm.`)
+}
+let calcularAreaTriangulo = () => {
+    const valueC = parseInt(inputC.value);
+    const altura = parseInt(inputH.value);
 
-console.log(`El perimetro de un triangulo es : ${perimetroTriangulo} cm`);
-
-console.log(`El area de un triangulo es : ${areaTriangulo} cm2`);
-
+    let area = areaTriangulo(valueC, altura);
+    alert(`El area de su triangulo es de ${area} cm2.`)
+}
 console.groupEnd();
 
 console.group('Circulo');
 
 const pi = Math.PI;
-const radioCirculo = 4;
-const diametroCirculo = radioCirculo * 2;
-const perimetroCirculo = diametroCirculo*pi
-const areaCirculo = (pi*radioCirculo*radioCirculo)/2;
 
-console.log(`Radio del circulo: ${radioCirculo} cm`);
+function diametroCirculo(radio) {
+    return radio * 2;
+}
 
-console.log(`El diametro del circulo: ${diametroCirculo} cm`);
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * pi;
+}
 
-console.log(`El perimetro del Circulo es : ${perimetroCirculo}cm`);
+function areaCirculo(radio) {
+    return (radio * radio) * pi;
+}
+// console.log(`Radio del circulo: ${radioCirculo} cm`);
 
-console.log(`El area del circulo es: ${areaCirculo} cm2`);
+// console.log(`El diametro del circulo: ${diametroCirculo} cm`);
 
+// console.log(`El perimetro del Circulo es : ${perimetroCirculo}cm`);
 
-
-
-
-
-
-
+// console.log(`El area del circulo es: ${areaCirculo} cm2`);
 
 console.groupEnd();
+
+
+
+// Triangulo Isoceles: Posee dos lados iguales y uno diferente. 
